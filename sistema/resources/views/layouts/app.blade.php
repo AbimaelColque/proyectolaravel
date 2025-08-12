660ac3bfc2e22e6b98e1760b348bfa573763a649
Abimael Colque <colqueabimael@gmail.com>
2025-08-12 11:56:00 -0400

    ocultando botones de categorias y productos en el login

--- sistema/resources/views/layouts/app.blade.php
+++ sistema/resources/views/layouts/app.blade.php
@@ -28,6 +28,7 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                @if(Auth::check())
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
@@ -38,7 +39,7 @@
                         </li>
 
                     </ul>
-
+                @endif
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
