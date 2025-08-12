8f0b67dd7059f406b7fa428ee1a3e21320baa252
Abimael Colque <colqueabimael@gmail.com>
2025-08-12 12:25:21 -0400

    corrigiendo la funcion editar de productos

--- sistema/app/Http/Controllers/ProductoController.php
+++ sistema/app/Http/Controllers/ProductoController.php
@@ -60,8 +60,8 @@ class ProductoController extends Controller
     public function edit($id): View
     {
         $producto = Producto::find($id);
-
-        return view('producto.edit', compact('producto'));
+        $categorias = Categoria::all();
+        return view('producto.edit', compact('producto','categorias'));
     }
 
     /**
