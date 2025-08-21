2aba5d6836fec759108e788b45519cec6a583d22
Abimael Colque <colqueabimael@gmail.com>
2025-08-20 22:30:17 -0400

    creando CRUD clientes

--- sistema/resources/views/cliente/form.blade.php
+++ sistema/resources/views/cliente/form.blade.php
@@ -13,12 +13,12 @@
         </div>
         <div class="form-group mb-2 mb20">
             <label for="telefono" class="form-label">{{ __('Telefono') }}</label>
-            <input type="text" name="telefono" class="form-control @error('telefono') is-invalid @enderror" value="{{ old('telefono', $cliente?->telefono) }}" id="telefono" placeholder="Telefono">
+            <input type="number" name="telefono" class="form-control @error('telefono') is-invalid @enderror" value="{{ old('telefono', $cliente?->telefono) }}" id="telefono" placeholder="Telefono">
             {!! $errors->first('telefono', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
         <div class="form-group mb-2 mb20">
             <label for="email" class="form-label">{{ __('Email') }}</label>
-            <input type="text" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email', $cliente?->email) }}" id="email" placeholder="Email">
+            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email', $cliente?->email) }}" id="email" placeholder="Email">
             {!! $errors->first('email', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
         <div class="form-group mb-2 mb20">
