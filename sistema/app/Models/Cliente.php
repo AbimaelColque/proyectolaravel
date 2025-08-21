fc8d7fd79bf6f750e31269b75c2f50e89a8a3d39
Abimael Colque <colqueabimael@gmail.com>
2025-08-20 20:36:57 -0400

    creando controlador de clientes

---
+++ sistema/app/Models/Cliente.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Cliente
+ *
+ * @property $id
+ * @property $nombre
+ * @property $contacto
+ * @property $telefono
+ * @property $email
+ * @property $direccion
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property Venta[] $ventas
+ * @property Venta[] $ventas
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Cliente extends Model
+{
+    
+    protected $perPage = 20;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['nombre', 'contacto', 'telefono', 'email', 'direccion'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function ventas()
+    {
+        return $this->hasMany(\App\Models\Venta::class, 'id', 'cliente_id');
+    }
+    
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function ventas()
+    {
+        return $this->hasMany(\App\Models\Venta::class, 'id', 'cliente_id');
+    }
+    
+}
